9d429172125659a27b76f290f68f564b3e892862
Juslin Jukka <jusju@haaga-helia.fi>
2021-09-01 09:40:10 +0300

    ylevero tehtava

---
+++ ohjelmointi1/src/viikko2/Ylevero.java
@@ -0,0 +1,38 @@
+package viikko2;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Ylevero {
+
+	public static void main(String[] args) {
+		// SKANNERIN LUONTI
+		Scanner input = new Scanner(System.in);
+		// DECIMALFORMATTERIN LUONTI KAHDEN DESIMAALIN TARKKUUDELLE
+		DecimalFormat desimaalit = new DecimalFormat("0.00");
+		
+		System.out.print("Anna vuositulosi ja ikäsi: ");
+		// SEURAAVAKSI PITÄISI LUKEA KAKSI LUKUA , JOTKA SYÖTETÄÄN PERÄKKÄIN VÄLILYÖNTI VÄLISSÄ
+		double vuositulo = input.nextDouble();
+		int ika = input.nextInt();
+		
+		double ylevero = 0;
+		if(ika < 18) {
+			ylevero = 0;
+		} else {
+			ylevero = 0.0068 * vuositulo;
+			// JOS YLEVERO ON YLI 140 LEIKATAAN YLIMENEVÄ POIS
+			if(ylevero > 140) {
+				ylevero = 140;
+			} else if(ylevero < 70)  {
+				ylevero = 0;
+			}
+		}
+		
+		System.out.println("Ylevero on " + desimaalit.format(ylevero));
+		// Anna vuositulosi ja ikäsi: 9000 21
+		// Ylevero on 0,00
+
+	}
+
+}
