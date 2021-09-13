8f1a9d19b3f90135baa3ce9a01adf4cb93c06689
Juslin Jukka <jusju@haaga-helia.fi>
2021-09-13 09:56:02 +0300

    first

---
+++ ohjelmointi1/src/viikko4/MerkkijonojenVertailu.java
@@ -0,0 +1,19 @@
+package viikko4;
+
+import java.util.Scanner;
+
+public class MerkkijonojenVertailu {
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Syötä kaksi merkkijonoa:");
+        String eka = lukija.nextLine();
+        String toka = lukija.nextLine();
+
+        if (eka.equals(toka) == true) {
+            System.out.println("Syötetyt merkkijonot olivat samat!");
+        } else {
+            System.out.println("Syötetyt merkkijonot eivät olleet samat!");
+        }
+    }
+}
